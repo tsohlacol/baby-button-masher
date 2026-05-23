ecd1015be52841263bc11a34855ec2799c934e90
Aaron <atkatz@gmail.com>
2026-05-23 14:54:03 -0400

    auto: update App.xaml.cs

--- host/App.xaml.cs
+++ host/App.xaml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Linq;
 using System.Windows;
 
 namespace ToddlerScreenDefender
@@ -8,6 +10,20 @@ namespace ToddlerScreenDefender
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            if (e.Args.Contains("--debug", StringComparer.OrdinalIgnoreCase))
+            {
+                string logDir = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "ToddlerScreenDefender");
+                TsdLog.Init(Path.Combine(logDir, "tsd-debug.log"));
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            TsdLog.Write("Application exiting");
+            TsdLog.Close();
+            base.OnExit(e);
         }
     }
 }
