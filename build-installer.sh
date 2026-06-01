97edd590dc11159365bfe14a05e2cd81908ccbed
Aaron <atkatz@gmail.com>
2026-05-31 23:44:26 -0400

    auto: update build-installer.sh

--- build-installer.sh
+++ build-installer.sh
@@ -14,7 +14,6 @@ docker build -t bbm-pipeline -f Dockerfile.build --build-arg APP_VERSION="${VERS
 echo "==> Copying final Windows Installer wrapper straight to ./build-output/ ..."
 docker run --rm -v "$(pwd)/build-output:/output-directory" bbm-pipeline 2>&1 | tee -a logs/build-installer.log
 
-VERSION=$(node -p "require('./package.json').version")
 echo "========================================================="
 echo " SUCCESS: Windows Installer assembled!"
 echo " Output path: ./build-output/BBM_Setup_v${VERSION}.exe"
